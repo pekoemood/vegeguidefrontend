645590a3842986dba37a8ab7cb62a82cdbda21a5
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-04-06 10:19:23 +0900

    テスト用のフロントページ作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
+	const [todos, setTodos] = useState([]);
 
-  return (
-    <>
-      <h1 className="text-4xl text-center">hello world</h1>
-    </>
-  )
-}
+	useEffect(() => {
+		const getTodos = async () => {
+			const response = await fetch("http://localhost:3000");
+			const data = await response.json();
+			setTodos(data);
+		};
+		getTodos();
+	}, []);
 
-export default App
+	return (
+		<>
+			<div className="container mx-auto mt-8">
+				<h1 className="text-4xl text-center">hello world</h1>
+			</div>
+			<div className="grid grid-cols-4 gap-4">
+				{todos.map((todo) => (
+					<div key={todo.id} className="card w-96 bg-base-100 card-xl shadow-sm">
+						<div className="card-body">
+							<h2 className="card-title">{todo.title}</h2>
+							<p>{todo.description}</p>
+						</div>
+					</div>
+				))}
+			</div>
+		</>
+	);
+};
+
+export default App;
