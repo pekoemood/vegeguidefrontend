ee45c61c1930d785938c93fde2575cd56453cbd0
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-04-13 16:31:23 +0900

    開発前の最終コミット

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
 import { useEffect, useState } from "react";
 
 const App = () => {
-	const [todos, setTodos] = useState([]);
+	const [data, setData] = useState(null);
 
 	useEffect(() => {
 		const getTodos = async () => {
-			const response = await fetch(`${import.meta.env.VITE_RAILS_API}`);
-			const data = await response.json();
-			setTodos(data);
+			const response = await fetch(`${import.meta.env.VITE_RAILS_API}/vegetables`);
+			const jsonData = await response.json();
+			setData(jsonData);
 		};
 		getTodos();
 	}, []);
 
+	if(!data) {
+		return <p>読み込み中....</p>
+	}
+
+	console.log(data.data.attributes);
 	return (
 		<>
-			<div className="container mx-auto mt-8">
-				<h1 className="text-4xl text-center">hello world</h1>
-			</div>
-			<div className="grid grid-cols-4 gap-4">
-				{todos.map((todo) => (
-					<div key={todo.id} className="card w-96 bg-base-100 card-xl shadow-sm">
-						<div className="card-body">
-							<h2 className="card-title">{todo.title}</h2>
-							<p>{todo.description}</p>
-						</div>
-					</div>
-				))}
-			</div>
+      <h1>野菜一覧</h1>
+			<p>{data.data[0].attributes.name}</p>
+			<p>{data.included[0].attributes.price}</p>
+		
 		</>
 	);
 };
