2535e99d752b3b96cb25ee3f59eb55896163e9ff
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-04-20 19:16:34 +0900

    vegeDetailのAPIレスポンスを確認

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,12 +4,13 @@ import Login from "../pages/Login/Login";
 import Top from "../pages/Top/Top";
 import VegeList from "../pages/VegeList/VegeList";
 import { vegeListLoader } from "../pages/VegeList/vegeListLoader";
+import VegeDetail from "../pages/VegeDetail/VegeDetail";
+import { vegeDetailLoader } from "../pages/VegeDetail/vegeDetailLoader";
 
 export const routes = createBrowserRouter([
 	{
 		Component: Layout,
 		hydrateFallbackElement: <p>Loading...</p>,
-
 		children: [
 			{
 				index: true,
@@ -23,6 +24,11 @@ export const routes = createBrowserRouter([
 				path: "/vegelist",
 				Component: VegeList,
 				loader: vegeListLoader,
+			},
+			{
+				path: "/vegelist/:id",
+				Component: VegeDetail,
+				loader: vegeDetailLoader,
 			},
 		],
 	},
