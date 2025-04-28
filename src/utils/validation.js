46134179b39b5a9e39c5c2cab5ce91afbc64ea0b
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-04-28 18:59:23 +0900

    新規登録、ログイン画面にバリデーションを追加

---
+++ src/utils/validation.js
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const signUpSchema = z.object({
+  name: z.string().min(1, '名前は必須です'),
+  email: z.string().email('正しいメールアドレスを入力してください'),
+  password: z.string().min(8, 'パスワードは8文字以上である必要があります'),
+});
+
+export const loginSchema = z.object({
+  email: z.string().email('正しいメールアドレスを入力してください'),
+  password: z.string().min(8, 'パスワードは8文字以上である必要があります'),
+});
