821fb934fa0916fc866ff84a478a5c704d339349
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-04-29 17:43:11 +0900

    vite.configを修正

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	server: {
+	server: process.env.NODE_ENV === 'production' ? {} : {
 		https: {
 			key: fs.readFileSync(path.resolve(__dirname, "certs/localhost-key.pem")),
 			cert: fs.readFileSync(path.resolve(__dirname, "certs/localhost.pem")),
