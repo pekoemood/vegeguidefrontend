3461a6c4cb4fabb89f768f31d2667e0851fc6ed5
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-05-08 00:03:33 +0900

    メモカード、レシピカードのレイアウトを統一

--- src/components/PageinationButtons.jsx
+++ src/components/PageinationButtons.jsx
@@ -1,27 +1,25 @@
 import { useSearchParams } from "react-router";
 
-const PaginationButtons = ({ totalPages, currentPage}) => {
+const PaginationButtons = ({ totalPages, currentPage }) => {
+	const [searchParams, setSearchParams] = useSearchParams();
 
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const handleMove = (newPage) => {
+	const handleMove = (newPage) => {
 		setSearchParams({ page: newPage });
 	};
 
-  return (
-    <div  className="flex justify-center join my-8">
-      {Array.from({ length: totalPages }, (v, i) => i + 1).map((page) => (
-        <button
-          key={page}
-          className={`join-item btn ${currentPage + 1 === page ? "btn-active" : ""}`}
-          onClick={() => handleMove(page)}
-        >
-          {page}
-        </button>
-      )
-      )}
-    </div>
-  );
+	return (
+		<div className="flex justify-center join my-8">
+			{Array.from({ length: totalPages }, (v, i) => i + 1).map((page) => (
+				<button
+					key={page}
+					className={`join-item btn ${currentPage + 1 === page ? "btn-active" : ""}`}
+					onClick={() => handleMove(page)}
+				>
+					{page}
+				</button>
+			))}
+		</div>
+	);
 };
 
 export default PaginationButtons;
--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -12,9 +12,9 @@ const RecipeCard = ({
 	ingredients,
 }) => {
 	return (
-		<Link to={`/recipe-lists/${id}`}>
-			<div className="card bg-base-100 w-90 shadow-sm transition-transform duration-200 ease-in-out transform hover:-translate-y-1 hover:shadow-lg p-4">
-				<div className="card-body">
+		<div className="card bg-base-100 w-90 shadow-sm transition-transform duration-200 ease-in-out transform hover:-translate-y-1 hover:shadow-lg p-4">
+			<div className="card-body space-y-3">
+				<Link to={`/recipe-lists/${id}`} className="block space-y-3">
 					<div className="flex justify-between items-center gap-2">
 						<h2 className="flex-auto card-title line-clamp-1">{title}</h2>
 						<span className="flex-none badge badge-secondary">
@@ -22,32 +22,36 @@ const RecipeCard = ({
 						</span>
 					</div>
 
-					<p className="text-neutral-500 line-clamp-2">{instructions}</p>
-					<div className="flex flex-col space-y-3">
-						<div className="flex items-center space-x-2">
+					<p className="text-neutral-500 line-clamp-2">
+						{instructions}
+					</p>
+
+					<div className="flex flex-col gap-2 text-neutral-700">
+						<div className="flex items-center gap-2">
 							<Clock />
 							<span>{cookingTime}分</span>
 						</div>
-						<div className="flex items-center space-x-2">
+						<div className="flex items-center gap-2">
 							<CookingPot />
 							<span className="line-clamp-1">
 								{ingredients.map((item) => item.name).join(" ")}
 							</span>
 						</div>
 					</div>
+				</Link>
 
-					<div className="mt-2 flex justify-end space-x-2">
-						<button className="btn btn-primary">
-							<ShoppingCart />
-							買い物リストに追加する
-						</button>
-						<button className="btn btn-error">
-							<Trash2 />
-						</button>
-					</div>
+				<div className="border-t border-base-300 pt-3 flex justify-around gap-2">
+					<button className="btn btn-primary flex items-center gap-2">
+						<ShoppingCart />
+						<span>買い物に追加</span>
+					</button>
+					<button className="btn btn-outline flex items-center gap-2">
+						<Trash2 />
+						<span>削除する</span>
+					</button>
 				</div>
 			</div>
-		</Link>
+		</div>
 	);
 };
 
