6cb2604757ee79e5633c42c8dcc3edab10b70ea4
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-05-21 23:12:07 +0900

    新しいアイテムを追加用のAddItemFormを作成

---
+++ src/components/AddItemForm.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const AddItemForm = ({ categories, closeModal }) => {
+	const [name, setName] = useState("");
+	const [amount, setAmount] = useState("");
+	const [category, setCategory] = useState("");
+	const [checked, setChecked] = useState(false);
+
+	return (
+		<>
+			<div className="bg-base-100 p-6 rounded-lg w-full max-w-lg shadow-lg">
+				<h2 className="text-lg font-bold mb-1">新しいアイテムを追加</h2>
+				<p className="text-sm text-neutral-500 mb-4">
+					買い物リストに追加するアイテムの情報を入力してください
+				</p>
+
+				<div className="grid grid-cols-3 gap-4 mb-4">
+					<div className="col-span-2">
+						<input
+							type="text"
+							className="input w-full"
+							placeholder="アイテム名"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+					</div>
+					<div className="col-span-1">
+						<input type="text" className="input" placeholder="数量" />
+					</div>
+				</div>
+
+				<div className="mb-4">
+					<select name="" id="" className="select">
+						<option value="" className="text-neutral-500">
+							カテゴリーを選択
+						</option>
+						{categories.map((cate) => (
+							<option key={cate} value={cate}>
+								{cate}
+							</option>
+						))}
+					</select>
+				</div>
+
+				<div className="flex justify-center gap-2 mt-4">
+					<button className="btn" onClick={closeModal}>
+						キャンセル
+					</button>
+					<button className="btn" onClick={closeModal}>
+						追加
+					</button>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default AddItemForm;
--- src/pages/ShoppingListDetail/ShoppingListDetail.jsx
+++ src/pages/ShoppingListDetail/ShoppingListDetail.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
-import { ShoppingCart, Trash2, Plus } from "lucide-react";
+import { Plus, ShoppingCart, Trash2 } from "lucide-react";
 import { useState } from "react";
 import { useLoaderData, useNavigate } from "react-router";
+import AddItemForm from "../../components/AddItemForm";
 import useModal from "../../hooks/useModal";
 
 const ShoppingListDetail = () => {
@@ -12,27 +13,28 @@ const ShoppingListDetail = () => {
 	const check = items.filter((item) => item.checked === true);
 	const navigate = useNavigate();
 	const [selectedCategory, setSelectedCategory] = useState(null);
+	const [newName, setNewName] = useState("");
 	const { Modal, openModal, closeModal } = useModal();
 	let filteredItems = items;
-	
-
-
+	console.log(items);
 
 	if (selectedCategory) {
-		filteredItems = filteredItems.filter((item) => item.category === selectedCategory)
+		filteredItems = filteredItems.filter(
+			(item) => item.category === selectedCategory,
+		);
 	}
 
 	const filteredGroupedItems = filteredItems.reduce((acc, item) => {
 		if (!acc[item.category]) {
-			acc[item.category] = []
+			acc[item.category] = [];
 		}
-		acc[item.category].push(item)
-		return acc
-	}, {})
+		acc[item.category].push(item);
+		return acc;
+	}, {});
 
 	Object.keys(filteredGroupedItems).forEach((category) => {
-		filteredGroupedItems[category].sort((a, b) => a.checked - b.checked)
-	})
+		filteredGroupedItems[category].sort((a, b) => a.checked - b.checked);
+	});
 
 	const groupedItems = items.reduce((acc, item) => {
 		if (!acc[item.category]) {
@@ -57,7 +59,6 @@ const ShoppingListDetail = () => {
 	const categories = Object.keys(groupedItems).sort((a, b) => {
 		return preferredOrder.indexOf(a) - preferredOrder.indexOf(b);
 	});
-
 
 	const handleClick = (id) => {
 		setItems((prev) =>
@@ -99,14 +100,24 @@ const ShoppingListDetail = () => {
 			<div className="px-4 pb-2">
 				<div className="flex flex-col space-y-2">
 					<div className="flex space-x-2">
-						<button className={`badge ${selectedCategory === null && 'badge-neutral'}`} onClick={() => setSelectedCategory(null)}>すべてのカテゴリ</button>
+						<button
+							className={`badge ${selectedCategory === null && "badge-neutral"}`}
+							onClick={() => setSelectedCategory(null)}
+						>
+							すべてのカテゴリ
+						</button>
 						{categories.map((category) => (
-							<button key={category} className={`badge ${selectedCategory === category && 'badge-neutral'}`} onClick={() => setSelectedCategory(category)}>{category}</button>
+							<button
+								key={category}
+								className={`badge ${selectedCategory === category && "badge-neutral"}`}
+								onClick={() => setSelectedCategory(category)}
+							>
+								{category}
+							</button>
 						))}
 					</div>
 				</div>
 			</div>
-
 
 			<div className="flex flex-col gap-6">
 				<div className="shadow-md rounded-xl p-4">
@@ -142,40 +153,42 @@ const ShoppingListDetail = () => {
 					</div>
 
 					<div className="mt-6">
-						{Object.entries(filteredGroupedItems).map(([category, categoryItems]) => (
-							<div key={category}>
-								<div className="flex items-center my-2">
-									<h3 className="text-sm text-neutral-500">{category}</h3>
-									<div className="flex-1 ml-2 h-0.5 bg-base-300" />
-								</div>
-								<ul className="space-y-2">
-									{categoryItems.map((item) => (
-										<li key={item.id}>
-											<div className="flex items-center justify-between space-x-4 py-4 px-4 rounded-lg border border-base-300">
-												<div className="flex items-center space-x-4">
-													<input
-														type="checkbox"
-														className="checkbox"
-														checked={item.checked}
-														onChange={() => handleClick(item.id)}
-													/>
-													<div className="flex flex-col">
-														<label htmlFor="">{item.name}</label>
-														<span className="text-neutral-500 text-sm">
-															レシピ: {item.fromRecipe}
-														</span>
+						{Object.entries(filteredGroupedItems).map(
+							([category, categoryItems]) => (
+								<div key={category}>
+									<div className="flex items-center my-2">
+										<h3 className="text-sm text-neutral-500">{category}</h3>
+										<div className="flex-1 ml-2 h-0.5 bg-base-300" />
+									</div>
+									<ul className="space-y-2">
+										{categoryItems.map((item) => (
+											<li key={item.id}>
+												<div className="flex items-center justify-between space-x-4 py-4 px-4 rounded-lg border border-base-300">
+													<div className="flex items-center space-x-4">
+														<input
+															type="checkbox"
+															className="checkbox"
+															checked={item.checked}
+															onChange={() => handleClick(item.id)}
+														/>
+														<div className="flex flex-col">
+															<label htmlFor="">{item.name}</label>
+															<span className="text-neutral-500 text-sm">
+																レシピ: {item.fromRecipe}
+															</span>
+														</div>
+													</div>
+													<div className="flex items-center space-x-4">
+														<span>{item.display_amount}</span>
+														<Trash2 className="hover:text-error" size={15} />
 													</div>
 												</div>
-												<div className="flex items-center space-x-4">
-													<span>{item.display_amount}</span>
-													<Trash2 className="hover:text-error" size={15} />
-												</div>
-											</div>
-										</li>
-									))}
-								</ul>
-							</div>
-						))}
+											</li>
+										))}
+									</ul>
+								</div>
+							),
+						)}
 					</div>
 					<div className="mt-6 flex justify-center">
 						<button
@@ -189,35 +202,7 @@ const ShoppingListDetail = () => {
 			</div>
 
 			<Modal>
-				<div className="bg-base-100 p-6 rounded-lg w-full max-w-lg shadow-lg">
-					<h2 className="text-lg font-bold mb-1">新しいアイテムを追加</h2>
-					<p className="text-sm text-neutral-500 mb-4">
-						買い物リストに追加するアイテムの情報を入力してください
-					</p>
-
-					<div className="grid grid-cols-3 gap-4 mb-4">
-						<div className="col-span-2">
-							<input type="text" className="input w-full" placeholder="アイテム名" />
-						</div>
-						<div className="col-span-1">
-							<input type="text" className="input" placeholder="数量" />
-						</div>
-					</div>
-
-					<div className="mb-4">
-						<select name="" id="" className="select">
-							<option value="" className="text-neutral-500">カテゴリーを選択</option>
-							{preferredOrder.map((cate) => (
-								<option key={cate} value={cate}>{cate}</option>
-							))}
-						</select>
-					</div>
-
-					<div className="flex justify-center gap-2 mt-4">
-						<button className="btn" onClick={closeModal}>キャンセル</button>
-						<button className="btn" onClick={closeModal}>追加</button>
-					</div>
-				</div>
+				<AddItemForm categories={preferredOrder} closeModal={closeModal} />
 			</Modal>
 		</div>
 	);
