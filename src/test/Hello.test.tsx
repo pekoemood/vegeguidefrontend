276386977bc16d3c3c6104e7714695eef0358a15
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-07-26 23:38:33 +0900

    Cardコンポーネントのテスト実装

--- src/test/Hello.test.tsx
+++ src/test/Hello.test.tsx
@@ -1,6 +1,8 @@
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-import Hello from './../components/Hello';
+import Hello from '../components/Hello';
+import React from 'react';
 
 describe('Hello Component', () => {
   it('should render "Hello, Vitest! text', () => {
