2a5b57a5906890de4f25f1ccfaa951cde9bf4dad
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-09-14 23:01:04 +0900

    feat: AddItemFormのhandleAddItem関数の型を修正
    - handleAddItemの引数にitemオブジェクトの型を追加
    - テストケースの名称を修正し、食材追加機能のテストを強化
    - 食材追加時にhandleAddItemが正しく呼ばれることを確認するテストを追加
    - キャンセルボタンが呼ばれることを確認するテストを追加

--- src/test/components/AddItemForm.test.tsx
+++ src/test/components/AddItemForm.test.tsx
@@ -19,12 +19,40 @@ describe('AddItemFrom', () => {
       render(<AddItemForm {...defaultProps} />)
     });
 
-    it('正しく表示されていること', () => {
+    it('必要な入力フィールドとボタンがすべて表示', () => {
       expect(screen.getByPlaceholderText('食材名')).toBeInTheDocument();
       expect(screen.getByPlaceholderText('数量')).toBeInTheDocument();
       expect(screen.getByRole('option', { name: 'カテゴリーを選択' })).toBeInTheDocument();
       expect(screen.getByRole('button', { name: 'キャンセル' })).toBeInTheDocument();
       expect(screen.getByRole('button', { name: '追加' })).toBeInTheDocument();
-    })
-  })
+    });
+
+    it('食材を追加できること', async () => {
+      const user = userEvent.setup();
+      const ingredientName = screen.getByPlaceholderText('食材名');
+      await user.type(ingredientName, '人参');
+      const amountInput = screen.getByPlaceholderText('数量');
+      await user.type(amountInput, '1本');
+      const categoryElement = screen.getByRole('combobox');
+      await user.selectOptions(categoryElement, '野菜');
+
+      const addButton = screen.getByRole('button', { name: '追加'});
+      await user.click(addButton);
+      expect(defaultProps.handleAddItem).toBeCalled();
+      expect(defaultProps.handleAddItem).toHaveBeenCalledTimes(1);
+      expect(defaultProps.handleAddItem).toHaveBeenCalledWith({
+        name: '人参',
+        display_amount: '1本',
+        category: '野菜',
+      });
+    });
+
+    it('キャンセルボタンが呼ばれること', async () => {
+      const user = userEvent.setup();
+      const cancelButton = screen.getByRole('button', { name: 'キャンセル'});
+      await user.click(cancelButton);
+      expect(defaultProps.closeModal).toBeCalled();
+      expect(defaultProps.closeModal).toHaveBeenCalledTimes(1);
+    });
+  });
 })
